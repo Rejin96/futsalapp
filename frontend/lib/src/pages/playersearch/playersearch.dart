fb0be94aa9d99a793e535f8e902434e1729047d3
sumandhami <sumandhami86@gmail.com>
2025-03-09 17:12:28 +0545

    added some feature in player recommendation

--- frontend/lib/src/pages/playersearch/playersearch.dart
+++ frontend/lib/src/pages/playersearch/playersearch.dart
@@ -25,33 +25,27 @@ class _PlayersearchState extends State<Playersearch> {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('auth_token');
     String? csrfToken = prefs.getString('csrf_token');
-    if (token == null) {
-      print('Token missing!');
-      return;
-    }
-    if (csrfToken == null) {
+    if (token == null || csrfToken == null) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('CSRF token is missing!')),
+        SnackBar(content: Text('Authentication token missing!')),
       );
       return;
     }
     try {
-      final response =
-          await http.get(Uri.parse(apiUrls["playerrecommendation"]!),
-           headers: {
-        'Authorization': 'Bearer $token',
-        'Content-Type': 'application/json',
-        'X-CSRFToken': csrfToken,
-        'Cookie': 'csrftoken=$csrfToken',
-      },
-          );
-          
+      final response = await http.get(
+        Uri.parse(apiUrls["playerrecommendation"]!),
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrfToken,
+          'Cookie': 'csrftoken=$csrfToken',
+        },
+      );
 
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
-
         setState(() {
-          players = data['recommended_players']; // Corrected key
+          players = data['recommended_players'];
           isLoading = false;
         });
       } else {
@@ -70,24 +64,15 @@ class _PlayersearchState extends State<Playersearch> {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(
-            Icons.arrow_back,
-            color: Colors.grey.shade300,
-          ),
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(Icons.arrow_back, color: Colors.grey.shade300),
         ),
-        title: Text(
-          "Recommended Players",
-          style: TextStyle(color: Colors.grey.shade300),
-        ),
+        title: Text("Recommended Players", style: TextStyle(color: Colors.grey.shade300)),
         backgroundColor: Color(0xFF1B2A41),
       ),
       body: SafeArea(
         child: Stack(
           children: [
-            // Background gradient
             Container(
               width: double.infinity,
               height: double.infinity,
@@ -95,11 +80,7 @@ class _PlayersearchState extends State<Playersearch> {
                 gradient: RadialGradient(
                   center: Alignment.center,
                   radius: 0.9,
-                  colors: [
-                    Color(0xFF1B2A41),
-                    Color(0xFF23395B),
-                    Color(0xFF2D4A69),
-                  ],
+                  colors: [Color(0xFF1B2A41), Color(0xFF23395B), Color(0xFF2D4A69)],
                   stops: [0.3, 0.7, 1.0],
                 ),
               ),
@@ -114,8 +95,7 @@ class _PlayersearchState extends State<Playersearch> {
                         ),
                       )
                     : ListView.builder(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 16.0, vertical: 20.0),
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
                         itemCount: players.length,
                         itemBuilder: (context, index) {
                           final player = players[index];
@@ -127,80 +107,83 @@ class _PlayersearchState extends State<Playersearch> {
                             ),
                             child: Padding(
                               padding: const EdgeInsets.all(12.0),
-                              child: Row(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  // Profile Picture
-                                  Container(
-                                    width: 60,
-                                    height: 60,
-                                    decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      border: Border.all(
-                                        color: Colors.white,
-                                        width: 2,
+                                  Row(
+                                    children: [
+                                      Container(
+                                        width: 60,
+                                        height: 60,
+                                        decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          border: Border.all(color: Colors.white, width: 2),
+                                          image: DecorationImage(
+                                            image: AssetImage('assets/images/futsaluser.jpeg'),
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
                                       ),
-                                      image: DecorationImage(
-                                        image: AssetImage(
-                                            'assets/images/futsaluser.jpeg'),
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(width: 16),
-
-                                  // Player Details
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          player['name'] ?? 'No Name',
-                                          style: TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                        const SizedBox(height: 4),
-                                        Text(
-                                          "Location: ${player['location'] ?? 'Unknown'}",
-                                          style: TextStyle(
-                                            fontSize: 14,
-                                            color: Colors.white70,
-                                          ),
-                                        ),
-                                        const SizedBox(height: 4),
-                                        Text(
-                                          "Contact: ${player['phone_number'] ?? 'N/A'}",
-                                          style: TextStyle(
-                                            fontSize: 14,
-                                            color: Colors.white70,
-                                          ),
-                                        ),
-                                        const SizedBox(height: 4),
-                                        Row(
+                                      const SizedBox(width: 16),
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
                                           children: [
-                                            Icon(
-                                              Icons.circle,
-                                              size: 12,
-                                              color: player['status'] ==
-                                                      'online'
-                                                  ? Colors.green
-                                                  : Colors.red,
-                                            ),
-                                            const SizedBox(width: 6),
                                             Text(
-                                              player['status'] ?? 'Unknown',
-                                              style: TextStyle(
-                                                fontSize: 14,
-                                                color: Colors.white70,
-                                              ),
+                                              player['name'] ?? 'No Name',
+                                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                                            ),
+                                            const SizedBox(height: 4),
+                                            Text(
+                                              "Location: ${player['location'] ?? 'Unknown'}",
+                                              style: TextStyle(fontSize: 14, color: Colors.white70),
+                                            ),
+                                            const SizedBox(height: 4),
+                                            Text(
+                                              "Contact: ${player['phone_number'] ?? 'N/A'}",
+                                              style: TextStyle(fontSize: 14, color: Colors.white70),
+                                            ),
+                                            const SizedBox(height: 4),
+                                            Row(
+                                              children: [
+                                                Icon(
+                                                  Icons.circle,
+                                                  size: 12,
+                                                  color: player['status'] == 'online' ? Colors.green : Colors.red,
+                                                ),
+                                                const SizedBox(width: 6),
+                                                Text(
+                                                  player['status'] ?? 'Unknown',
+                                                  style: TextStyle(fontSize: 14, color: Colors.white70),
+                                                ),
+                                              ],
                                             ),
                                           ],
                                         ),
-                                      ],
-                                    ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      ElevatedButton(
+                                        onPressed: () {},
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: Colors.redAccent,
+                                          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                                        ),
+                                        child: Text("Compete", style: TextStyle(color: Colors.white)),
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () {},
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: Colors.greenAccent,
+                                          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                                        ),
+                                        child: Text("Invite", style: TextStyle(color: Colors.black)),
+                                      ),
+                                    ],
                                   ),
                                 ],
                               ),
